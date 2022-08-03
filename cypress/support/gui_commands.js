db779ce7612117cb761090f7ddc3169cc62cccc3
julianokoslowisk <julianokoslowisk@gmail.com>
2022-08-03 10:20:39 -0300

    Correto agora

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,35 +1,34 @@
 /// <reference types="Cypress" />
 
 Cypress.Commands.add('login', () => {
-    cy.visit('users/sign_in')
-  
-    cy.get("[data-qa-selector='login_field']").type(Cypress.env('user_name'))
-    cy.get("[data-qa-selector='password_field']").type(Cypress.env('user_password'))
-    cy.get("[data-qa-selector='sign_in_button']").click()
-  })
+  cy.visit('users/sign_in')
 
-  Cypress.Commands.add('logout', () => {
-    cy.get('.qa-user-avatar').click()
-    cy.contains('Sign out').click()    
-  })
+  cy.get("[data-qa-selector='login_field']").type(Cypress.env('user_name'))
+  cy.get("[data-qa-selector='password_field']").type(Cypress.env('user_password'))
+  cy.get("[data-qa-selector='sign_in_button']").click()
+})
 
-  Cypress.Commands.add('gui_createProject', project => {
-    cy.visit('projects/new')
-  
-    cy.get('#project_name').type(project.name)
-    cy.get('#project_description').type(project.description)
-    cy.get('.qa-initialize-with-readme-checkbox').check()
-    cy.contains('Create project').click()
-  }) 
+Cypress.Commands.add('logout', () => {
+  cy.get('.qa-user-avatar').click()
+  cy.contains('Sign out').click()
+})
 
-  Cypress.Commands.add('gui_createIssue', issue => {
-    cy.visit(`${Cypress.env('user_name')}/${issue.project.name}/issues/new`)
-  
-    cy.get('.qa-issuable-form-title').type(issue.title)
-    cy.get('.qa-issuable-form-description').type(issue.description)
-    cy.contains('Submit issue').click()
-  })
-  
+Cypress.Commands.add('gui_createProject', project => {
+  cy.visit('projects/new')
+
+  cy.get('#project_name').type(project.name)
+  cy.get('#project_description').type(project.description)
+  cy.get('.qa-initialize-with-readme-checkbox').check()
+  cy.contains('Create project').click()
+})
+
+Cypress.Commands.add('gui_createIssue', issue => {
+  cy.visit(`${Cypress.env('user_name')}/${issue.project.name}/issues/new`)
+
+  cy.get('.qa-issuable-form-title').type(issue.title)
+  cy.get('.qa-issuable-form-description').type(issue.description)
+  cy.contains('Submit issue').click()
+})
 
 
-  
+
